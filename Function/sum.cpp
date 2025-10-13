7a82212a40aa7f00878f668f428c31bec9b1d7e0
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-10-13 17:37:34 +0500

    add function file

---
+++ Function/sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+   //first function declaration
+int sum();
+
+  //main funtiona
+int main(){
+    sum();
+    return 0;
+}
+
+//funtion 1 find sum
+int sum(){
+    int a,b,s;
+    cout<<"enter first number : ";
+    cin>>a;
+    cout<<"enter second number : ";
+    cin>>b;
+    
+    //sum 
+     s=a+b;
+     cout<<"sum is equall:"<<s<<endl;
+     return s;
+}
