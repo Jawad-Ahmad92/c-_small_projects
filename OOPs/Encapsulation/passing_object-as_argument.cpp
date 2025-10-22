b4faba475e25e09f664b6df82defce9604d49e4f
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-10-22 22:27:30 +0500

    in function passing object as a argument

---
+++ OOPs/Encapsulation/passing_object-as_argument.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Travel{
+    private:
+    int km,hr;
+    public:
+    Travel(){
+        km=hr=0;
+    }
+
+    int get(){
+        cout<<"enter kilometers :";
+        cin>>km;
+        cout<<"enter hours:";
+        cin>>hr;
+
+    }
+    int show(){
+        cout<<"you traveled: "<<km<<"km"<<" in "<<hr<<"hourse "<<endl;
+
+    }
+    Travel add(Travel p){  //p is a parameter
+        Travel T;   //T is object inside function
+        T.km=km+p.km;
+        T.hr=hr+p.hr;
+
+        return T;
+    }
+};
+int main(){
+    Travel my,your,r;
+    my.get();
+    my.show();
+    your.get();
+    your.show();
+    r=my.add(your);
+
+    cout<<"Total travilli is as follows:"<<endl;
+    r.show();
+
+    return 0;
+}
